5282b20f55ea8a18add3b2a03e931e9eb7a504c9
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-06-21 18:50:43 -0300

    Service layer, component registration

--- src/main/java/com/vitoriaferreira/curso/resource/UserResource.java
+++ src/main/java/com/vitoriaferreira/curso/resource/UserResource.java
@@ -1,22 +1,37 @@
 package com.vitoriaferreira.curso.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vitoriaferreira.curso.entities.User;
+import com.vitoriaferreira.curso.services.UserService;
 
-@RestController // anotação que indica que essa classe é um controlador REST
+@RestController // indica q classe é um controlador REST
 @RequestMapping(value = "/users") // mapeia a URL base para os recursos dessa classe
 public class UserResource {
 
-    // RespondeEntity é uma classe que encapsula a resposta HTTP
-    // O método findAll() deve retornar uma lista de usuários
+    // UserResource depende do UserService
+
+    @Autowired
+    private UserService service;
+
     @GetMapping // mapeia o método HTTP GET para o endpoint /users
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "000-0000", "123456");
-        return ResponseEntity.ok().body(u); // Retorna a resposta com o usuário criado
+    public ResponseEntity<List<User>> findAll() {// RespondeEntity é uma classe que encapsula a resposta HTTP
+        List<User> list = service.findAll(); // Chama o serviço para obter a lista de usuários
+        return ResponseEntity.ok().body(list); // Retorna a resposta com o usuário criado
+    }
+
+    // endpoint para buscar usuário por ID
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj); // Retorna a resposta com o usuário encontrado
     }
 
 }
---
+++ src/main/java/com/vitoriaferreira/curso/services/UserService.java
@@ -0,0 +1,27 @@
+package com.vitoriaferreira.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vitoriaferreira.curso.entities.User;
+import com.vitoriaferreira.curso.repositories.UserRepository;
+
+//temque ser registrado como componente pois  UserResource depende do UserService
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository; // Injeção de dependência do UserRepository
+
+    public List<User> findAll() {
+        return repository.findAll(); // repassa a chamada para o repositório
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
