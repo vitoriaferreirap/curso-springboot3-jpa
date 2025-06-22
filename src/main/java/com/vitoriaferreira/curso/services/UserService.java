4f71943bd2690c26bb75a00ce572d6f8f86f1ae4
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-06-22 15:18:39 -0300

    User insert

--- src/main/java/com/vitoriaferreira/curso/services/UserService.java
+++ src/main/java/com/vitoriaferreira/curso/services/UserService.java
@@ -24,4 +24,10 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
+
+    // operações
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
 }
